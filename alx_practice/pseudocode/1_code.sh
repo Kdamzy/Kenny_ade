148ff2f0ea2cdca4502857213c2bc3cc052adec9
Adeyemi0804 <abdulazeezadeyinka0804@gmail.com>
2023-07-28 16:23:46 +0100

    working clean

--- alx_practice/pseudocode/1_code.sh
+++ alx_practice/pseudocode/1_code.sh
@@ -1,9 +1,10 @@
-# function to check if a number is positive or negative
-read number from users; num
-is_positive() {
-let num = 1
+// function to check if a number is positive or negative
+read -p "number from users: " num
+function is_positive(){
+
 	if [num < 0]; then
 		echo "number num is negative"
 	else
 		echo "number num is positive"
-}
+	fi
+	}
